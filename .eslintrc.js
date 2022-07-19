33280625b986981e7df51dbcee607eb2630ae503
codewriter73 <mason.fd.c@gmail.com>
2022-07-19 01:35:36 -0400

    eslint airbnb for all formatting, prettier for vscode autoformatting

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'airbnb-base'],
+  rules: {
+    'linebreak-style': 0,
+  },
+  // parserOptions: {
+  //   project: './tsconfig.json',
+  // },
+};
