a0527722eb9eb69c2585df37eccc007cd73fdb5a
codewriter73 <mason.fd.c@gmail.com>
2022-07-20 00:07:41 -0400

    feat: added mantine ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['next/core-web-vitals', 'airbnb-base'],
   rules: {
     'linebreak-style': 0,
+    'no-restricted-exports': 0,
+    'import/extensions': ['error', 'never', { svg: 'always' }],
   },
   parserOptions: {
     project: './tsconfig.json',
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx,css,json}': ['eslint --fix', 'eslint'],
-    '**/*.ts?(x)': () => 'tsc --noEmit --pretty',
-  };
+  '*.{js,jsx,ts,tsx,css}': ['eslint --fix', 'eslint'],
+  '**/*.ts?(x)': () => 'tsc --noEmit --pretty',
+};
