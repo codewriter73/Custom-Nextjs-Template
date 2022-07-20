b53abbcc3195deb6d435fc9284ac88250b7ff16f
codewriter73 <mason.fd.c@gmail.com>
2022-07-19 20:30:38 -0400

    ci: added commitlint and commit-msg hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,5 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};
+    '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+    '**/*.ts?(x)': () => 'npm run build-types',
+    // '*.json': ['prettier --write'],
+  };
