4e8db2ac7e4db6b98a778124570dde4e9f7be745
codewriter73 <mason.fd.c@gmail.com>
2022-07-25 23:48:08 -0400

    build: added jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
+  root: true,
   extends: [
     'next/core-web-vitals',
     'airbnb-base',
     'plugin:cypress/recommended',
   ],
+  plugins: ['testing-library'],
   rules: {
     'linebreak-style': 0,
     'no-restricted-exports': 0,
     'import/extensions': ['error', 'never', { svg: 'always' }],
     'import/no-extraneous-dependencies': [
       'error',
-      { devDependencies: ['**/*.test.ts', '**/*.test.tsx', '**/*.cy.js', 'cypress.config.ts'] },
+      {
+        devDependencies: [
+          '**/*.test.ts',
+          '**/*.test.tsx',
+          '**/*.cy.js',
+          'cypress.config.ts',
+        ],
+      },
     ],
   },
   parserOptions: {
     project: './tsconfig.json',
   },
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
